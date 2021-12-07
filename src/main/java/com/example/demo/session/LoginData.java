26f5772393c7f6e04899b2d4629ebb085ca176b8
max1chernyaev <max1chernyaev@gmail.com>
2021-12-07 18:04:26 +0200

    added class LoginData added possibility to keep a couple "login" and "password" in HashMAp added checkLogList added try catch ConnectException

--- src/main/java/com/example/demo/session/LoginData.java
+++ src/main/java/com/example/demo/session/LoginData.java
@@ -7,12 +7,33 @@ public class LoginData {
 
 
     public static Map<String, String> logMap = new HashMap<>();
+    public static List<String[]> logList = new ArrayList<>();
 
     public static void logMap() {
         logMap.put("max", "123abc");
         logMap.put("ilon", "mask");
         logMap.put("thor", "from asgard");
     }
+
+    public static void logList(){
+
+        logList.add(new String[]{"max", "123abc"});
+        logList.add(new String[]{"ilon", "mask"});
+        logList.add(new String[]{"thor", "from asgard"});
+
+    }
+
+    public static boolean checkLogList(String login, String password){
+        for (String[] i:logList
+        ) {
+            if (i[0].equals(login) && i[1].equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 
 }
--- src/main/java/com/example/demo/session/LoginServlet.java
+++ src/main/java/com/example/demo/session/LoginServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.net.ConnectException;
 
 /**
  * Servlet implementation class LoginServlet
@@ -16,7 +16,7 @@ public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ConnectException {
 
         // Это название 2-х параметров, которые мы передаем
         String user = request.getParameter("user");
@@ -26,7 +26,27 @@ public class LoginServlet extends HttpServlet {
 
 
 
-        if (LoginData.logMap.containsKey(user) && LoginData.logMap.get(user).equals(pwd)) {
+        try {
+            if (LoginData.logMap.containsKey(user) && LoginData.logMap.get(user).equals(pwd)) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", "user");
+                //setting session to expiry in 30 mins
+                session.setMaxInactiveInterval(30 * 60);
+                Cookie userName = new Cookie("user", user);
+                userName.setMaxAge(30 * 60);
+                response.addCookie(userName);
+                PrintWriter out = response.getWriter();
+                out.println("Welcome back to the team, " + user + "!");
+            } else {
+                PrintWriter out = response.getWriter();
+                out.println("Either user name or password is wrong!");
+            }
+        }catch (ConnectException e){
+            e.printStackTrace();
+        }
+
+
+        /*if (LoginData.checkLogList(user, pwd)) {
             HttpSession session = request.getSession();
             session.setAttribute("user", "user");
             //setting session to expiry in 30 mins
@@ -39,7 +59,7 @@ public class LoginServlet extends HttpServlet {
         } else {
             PrintWriter out = response.getWriter();
             out.println("Either user name or password is wrong!");
-        }
+        }*/
 
     }
 }
