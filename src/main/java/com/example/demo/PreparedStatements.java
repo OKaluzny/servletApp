60f87c9991375cbac4f189ba6d3fb0bad7e3f05b
max1chernyaev <max1chernyaev@gmail.com>
2021-12-11 23:38:50 +0200

    create new class

--- src/main/java/com/example/demo/PreparedStatements.java
+++ src/main/java/com/example/demo/PreparedStatements.java
@@ -4,49 +4,54 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PreparedStatements {
-        public static PreparedStatement savePrepared;
+    public static PreparedStatement savePrepared;
+
     static {
-            try {
-                savePrepared = EmployeeConnection.Connect().prepareStatement("insert into users(name,email,country) values (?,?,?)");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        try {
+            savePrepared = EmployeeConnection.Connect().prepareStatement("insert into users(name,email,country) values (?,?,?)");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
 
-        public static PreparedStatement updatePrepared;
-        static {
-            try {
-                updatePrepared = EmployeeConnection.Connect().prepareStatement("update users set name=?,email=?,country=? where id=?");
-            }catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
+    public static PreparedStatement updatePrepared;
 
-        public static PreparedStatement deletePrepared;
-        static {
-            try {
-                deletePrepared = EmployeeConnection.Connect().prepareStatement("update from users where id=?");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    static {
+        try {
+            updatePrepared = EmployeeConnection.Connect().prepareStatement("update users set name=?,email=?,country=? where id=?");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
 
-        public static PreparedStatement getEmployeeById;
-        static {
-            try {
-                getEmployeeById = EmployeeConnection.Connect().prepareStatement("select * from users where id=?");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+    public static PreparedStatement deletePrepared;
 
-        public static PreparedStatement getAll;
-        static {
-            try{
-                getAll = EmployeeConnection.Connect().prepareStatement("select * from users");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    static {
+        try {
+            deletePrepared = EmployeeConnection.Connect().prepareStatement("update from users where id=?");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static PreparedStatement getEmployeeById;
+
+    static {
+        try {
+            getEmployeeById = EmployeeConnection.Connect().prepareStatement("select * from users where id=?");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PreparedStatement getAll;
+
+    static {
+        try {
+            getAll = EmployeeConnection.Connect().prepareStatement("select * from users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
